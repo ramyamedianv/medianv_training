d411aea49a9cef1e1a4c3266f6f48fc7f305a0a5
ramyamedianv <ramyakrishna@medianv.com>
2026-01-02 07:26:54 +0530

    Task 15:TypeORM + Realtime

--- TypeORM_Realtime/websocket-api/src/chat/chat-gateway.ts
+++ TypeORM_Realtime/websocket-api/src/chat/chat-gateway.ts
@@ -1,20 +1,44 @@
-import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { Socket,Server } from "socket.io";
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer
+} from "@nestjs/websockets";
+import { Socket, Server } from "socket.io";
 
-@WebSocketGateway(3002,{})
-export class ChatGateway implements OnGatewayConnection,OnGatewayDisconnect{
+@WebSocketGateway(3002, {})
+export class ChatGateway
+  implements OnGatewayConnection, OnGatewayDisconnect {
 
-    @WebSocketServer() server:Server;
-    handleConnection(client:Socket) {
-        console.log('New user connected...',client.id);
-        
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: Socket) {
+    console.log('New user connected...', client.id);
+     client.broadcast.emit('user-joined',{
+        message:`User Joined the chat ${client.id}`
     }
+     )
 
+  }
 
-    @SubscribeMessage('newMessage')
-    handleNewMessage(client:Socket,message){
-        console.log(message);
-        client.emit('reply','This is a reply')
-        this.server.emit('reply','broadcasting.....');
-    }
+  handleDisconnect(client: Socket) {
+    console.log('User disconnected...', client.id);
+    this.server.emit('user-left',{
+        message:`User left the chat ${client.id}`
+    });
+
+  }
+
+  @SubscribeMessage('newMessage')
+  handleNewMessage(
+    client: Socket,
+    @MessageBody() message: any
+  ) {
+    console.log(message);
+    client.emit('reply', 'This is a reply');
+    this.server.emit('reply', 'broadcasting.....');
+  }
 }
